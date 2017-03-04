c16346367258689d4e45360e5d14a3f61c83d84b
Ralf Schandl <rakus@users.noreply.github.com>
2017-03-04 19:56:34 +0100

    INSTALL.sh now requires parameter "YES"
    Only when "YES" is given, the installation is executed.
    Without parameter it runs in "simulation mode" (aka only says what would
    be done).
    
    Also: Don't copy if identical file is already installed.

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,23 +2,68 @@
 #
 # FILE: INSTALL.sh
 #
-# ABSTRACT: 
+# ABSTRACT: Installes Quick Change Directory
+#
+# Usage:
+#   INSTALL.sh
+#     If called without parameter, just prints out what would be done.
+#     AKA: Simulation mode.
+#
+#   INSTALL.sh YES
+#     If called with parameter "YES" it acually does what it says.
 #
 # AUTHOR: Ralf Schandl
 #
 # CREATED: 2017-03-03
 #
 
+set -u
+
 script_dir=$(cd "$(dirname $0)" 2>/dev/null; pwd)
 script_name="$(basename "$0")"
 script_file="$script_dir/$script_name"
 
-usage()
+
+EXECUTE=''
+if [ $# -gt 0 ]; then
+    if [ "$1" = "YES" ]; then
+        EXECUTE=true
+    else
+        echo "ERROR: unknown parameter $1"
+        echo ""
+        echo "USAGE: $script_name [YES]"
+        echo ""
+        echo "  If called WITHOUT paramater "YES", just print what would be done."
+        echo ""
+        echo "  If called with paramater "YES", do the installation."
+        echo ""
+        exit 1
+    fi
+fi
+
+
+simu_msg()
 {
-    echo >&2 "Installs Quick Change Directory files in your hom directory"
-    echo >&2 "Usage: INSTALL.sh [-f]"
-    echo >&2 "    -f  Force! Overwrite already existing files"
-    echo >&2 ""
+    echo "========================================================="
+    echo "  S I M U L A T I O N   M O D E"
+    echo ""
+    echo "  No commands will be executed."
+    echo ""
+    echo "  For real execution run: $0 YES "
+    echo ""
+    echo "========================================================="
+}
+
+run_cmd()
+{
+    typeset RC
+    if [ $EXECUTE ]; then
+        $@
+        return $?
+    else
+        echo "$@"
+        return 0
+    fi
 }
 
 copy_file()
@@ -26,24 +71,36 @@ copy_file()
     typeset src="$1"
     typeset tgt="$2"
 
+    if diff "$src" "$tgt" >/dev/null 2>&1; then
+        echo "SKIPPED: Identical $src already installed"
+        return 0
+    fi
+
     if [ -e "$tgt" ]; then
         echo "Saving existing $tgt to $tgt.install-prev"
-        [ ! $NOOP ] && mv -f "$tgt" "$tgt.install-prev"
+        run_cmd mv -f "$tgt" "$tgt.install-prev"
+        [ $? -ne 0 ] && exit 1
     fi
 
     echo "Copying $src to $tgt..."
-    [ ! $NOOP ] && cp -f "$src" "$tgt"
+    run_cmd cp -f "$src" "$tgt"
+    [ $? -ne 0 ] && exit 1
 }
 
 add_to_crontab()
 {
     if crontab -l | grep "qc-process-idx-list.sh" >/dev/null 2>&1; then
-        echo "Crontab entry exists -- skipping"
+        echo "SKIPPED: Crontab entry seems to exist"
     else
-        CRE="*/10 * * * * ${HOME}/.qc/qc-process-idx-list.sh >/dev/null 2>&1"
+        CRE="*/10 * * * * \${HOME}/.qc/qc-process-idx-list.sh >\${HOME}/.qc/qc-process-idx-list.log 2>&1"
         echo "Installing crontab entry:"
         echo "    $CRE"
-        [ ! $NOOP ] && (crontab -l 2>/dev/null; echo "$CRE") | crontab -
+        # to complex for run_cmd
+        if [ $EXECUTE ]; then
+            (crontab -l 2>/dev/null; echo "$CRE") | crontab -
+        else
+            echo "(crontab -l 2>/dev/null; echo \"$CRE\") | crontab -"
+        fi
     fi
 }
 
@@ -51,31 +108,30 @@ add_to_init_file()
 {
     typeset file="$1"
     if grep "\\.quick_change_dir" "$file" >/dev/null 2>&1; then
-        echo "The file .quick_change_dir seems to be already sourced from $file -- SKIPPING"
+        echo "SKIPPED: The file .quick_change_dir seems to be already sourced from $file"
     else
         echo "Adding .quick_change_dir to $file..."
-        [ ! $NOOP ] && echo ". $HOME/.quick_change_dir" >> $file
+        # to complex for run_cmd (because of redir)
+        if [ $EXECUTE ]; then
+            echo "[ -e \"\$HOME/.quick_change_dir\" ] && . \$HOME/.quick_change_dir" >> $file
+        else
+            echo "echo \"[ -e \"\$HOME/.quick_change_dir\" ] && . \$HOME/.quick_change_dir\" >> $file"
+        fi
     fi
 }
 
-NOOP=
-while getopts ":n" o "$@"; do
-    case $o in
-        n) NOOP=true
-            ;;
-        *)
-            [ "${!OPTIND}" != "--help" ] && echo >&2 "can't parse: ${!OPTIND}" && echo >&2 ""
-            usage
-            ;;
-    esac
-done
+#---------[ MAIN ]-------------------------------------------------------------
 
+[ ! $EXECUTE ] && simu_msg
+
+cd "$script_dir"
+[ $? -ne 0 ] && exit 1
 
 TGT="$HOME/.qc"
 
 # Create ~/.qc
 if [ ! -d "$TGT" ]; then
-    [ ! $NOOP ]mkdir "$TGT"
+    run_cmd mkdir "$TGT"
     [ $? -ne 0 ] && exit 1
 fi
 
@@ -86,21 +142,28 @@ copy_file _quick_change_dir "$HOME/.quick_change_dir"
 if [ ! -e "$TGT/qc-index.list" ]; then
     copy_file qc-index.list "$TGT/qc-index.list"
 else
-    copy_file qc-index.list "$TGT/qc-index.list.install-new"
-    echo "------------------------------------------------------------"
-    echo ""
-    echo "New Index Definition list copied to "
-    echo "      $TGT/qc-index.list.install-new"
-    echo ""
-    echo "Please validate the existing $TGT/qc-index.list and delete"
-    echo "qc-index.list.install-new afterwards."
-    echo ""
-    echo "------------------------------------------------------------"
+    if diff qc-index.list "$TGT/qc-index.list" >/dev/null 2>&1; then
+        echo "SKIPPED: Identical qc-index.list already installed"
+    else
+        copy_file qc-index.list "$TGT/qc-index.list.install-new"
+        echo "------------------------------------------------------------"
+        echo ""
+        echo "New Index Definition list copied to "
+        echo "      $TGT/qc-index.list.install-new"
+        echo ""
+        echo "Please validate the existing $TGT/qc-index.list and delete"
+        echo "qc-index.list.install-new afterwards."
+        echo ""
+        echo "------------------------------------------------------------"
+    fi
 fi
 
 add_to_crontab 
 
 add_to_init_file "$HOME/.bashrc"
 
+# Add to .kshrc if available?
+
+[ ! $EXECUTE ] && simu_msg
 
 #---------[ END OF FILE INSTALL.sh ]-------------------------------------------
