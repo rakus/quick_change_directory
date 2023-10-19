3b083eb54ff64ed3459c26b8691a37d7bf241665
Ralf Schandl <rakus@users.noreply.github.com>
2023-10-19 20:43:35 +0200

    Use getopt instead of shell-internal getopts
    Now long options (like --help) are properly handled.
    With getopts this was done with a "hack".
    
    The shell-internal getopts is still used in qc_mini and by
    qc-build-index when parsing the index definitions in qc-index.cfg.
    In both cases no long options are supported.

--- test/test-invopt.sh
+++ test/test-invopt.sh
@@ -8,6 +8,8 @@
 #
 # CREATED: 2021-03-12
 #
+# shellcheck disable=SC2059
+#
 
 script_dir="$(cd "$(dirname "$0")" && pwd)" || exit 1
 
@@ -16,41 +18,57 @@ set -u
 # shellcheck source=./defines.shinc
 . "${script_dir}/defines.shinc"
 
+#
+# Adjust to different versions of /bin/getopt
+# Maybe it is a bad idea to check for this error messages
+#
+if getopt "o" -D 2>&1 | grep -q invalid; then
+    # "newer version e.g 2.37.4
+    invShortOpt="invalid option -- '%c'"
+    invLongOpt="unrecognized option '--%s'"
+else
+    invShortOpt="unknown option -- %c"
+    invLongOpt="unknown option -- %s"
+fi
+
+
+
+
 startTest "Invalid option detection"
 
 printf "qc -Z"
-if qc-backend -Z 2>&1 | grep -q "Invalid option 'Z'"; then
+if qc-backend -Z 2>&1 | grep -q "$(printf "$invShortOpt" Z)"; then
     OK
 else
     ERROR
 fi
 printf "dstore -Z"
-if dstore -Z 2>&1 | grep -q "Invalid option 'Z'"; then
+if dstore -Z 2>&1 | grep -q "$(printf "$invShortOpt" Z)"; then
     OK
 else
     ERROR
 fi
 printf "qc-build-index -Z"
-if qc-build-index -Z 2>&1 | grep -q "Invalid option 'Z'"; then
+if qc-build-index -Z 2>&1 | grep -q "$(printf "$invShortOpt" Z)"; then
     OK
 else
     ERROR
 fi
 
 printf "qc --wrong"
-if qc-backend --wrong 2>&1 | grep -q "Invalid option '--wrong'"; then
+if qc-backend --wrong 2>&1 | grep -q "$(printf "$invLongOpt" wrong)"; then
     OK
 else
     ERROR
 fi
 printf "dstore --wrong"
-if dstore --wrong 2>&1 | grep -q "Invalid option '--wrong'"; then
+if dstore --wrong 2>&1 | grep -q "$(printf "$invLongOpt" wrong)"; then
     OK
 else
     ERROR
 fi
 printf "qc-build-index --wrong"
-if qc-build-index --wrong 2>&1 | grep -q "Invalid option '--wrong'"; then
+if qc-build-index --wrong 2>&1 | grep -q "$(printf "$invLongOpt" wrong)"; then
     OK
 else
     ERROR
